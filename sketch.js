bea9283525405bdac24d8b614d31b84251c78d94
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-10-26 13:20:32 +0800

    Implement UI for canvas size and resolution customization.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,81 @@
-function initializeCanvas() {
-    for (let i = 0; i < 16; i++) {
+function initializeCanvas(width, height, resolution) {
+
+    console.log(width, height, resolution);
+
+    modal.style.display = 'none';
+
+    const masterContainer = document.querySelector('.master-container')
+    masterContainer.style.width = width + 'px'
+    masterContainer.style.height = height + 'px'
+
+    for (let i = 0; i < resolution; i++) {
         
         let tempContainDiv = document.createElement('div');
         tempContainDiv.classList.add('divContainPixel');
         masterContainer.appendChild(tempContainDiv);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < resolution; j++) {
             let tempDiv = document.createElement("div");
             tempDiv.classList.add('divPixel');
             tempContainDiv.appendChild(tempDiv);
         }
     }
-    activateCanvas();
 }
 
+function clearCanvas() {
+    const divContainers = document.querySelectorAll('.divContainPixel');
+    
+    divContainers.forEach(div => {
+        while (div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+        masterContainer.removeChild(div);
+    });
 
-function activateCanvas() {
-    const allDivPixels = document.querySelectorAll('.divPixel');
-
-    masterContainer.addEventListener('mouseover', (event) => {
-        let target = event.target;
-        console.log(target);
-        target.classList.add('colored');
-    })
+    console.log('divs cleared');
 }
 
+// function takeUserCanvas() {
+//     modal.style.display = 'block';
 
-function takeUserCanvas() {
+//     const modalWidth = document.querySelector('#canvasWidth');
+//     const modalHeight = document.querySelector('#canvasHeight');
+//     const modalResolution = document.querySelector('#canvasResolution');
 
-}
+//     canvasWidth = modalWidth.value;
+//     canvasHeight = modalHeight.value;
+//     canvasResolution = modalResolution.value;    
+// }
 
 const activateButton = document.querySelector('.activate');
 const masterContainer = document.querySelector('.master-container');
+const modal = document.querySelector('.modal-container');
+const modalButton = document.querySelector('.submitButton');
+const modalWidth = document.querySelector('#canvasWidth');
+const modalHeight = document.querySelector('#canvasHeight');
+const modalResolution = document.querySelector('#canvasResolution');
 
+let canvasWidth = 600;
+let canvasHeight = 600;
+let canvasResolution = 16;
+
+initializeCanvas(canvasWidth, canvasHeight, canvasResolution);
+
+// display the modal if button is clicked
 activateButton.addEventListener('click', () => {
-    initializeCanvas();
+    modal.style.display = 'block';
 });
 
+// the modal is displayed; clicking the button takes the user input values
+modalButton.addEventListener('click', () => {
+    canvasWidth = modalWidth.value;
+    canvasHeight = modalHeight.value;
+    canvasResolution = modalResolution.value; 
+    clearCanvas();
+    initializeCanvas(canvasWidth, canvasHeight, canvasResolution);
+});
 
+masterContainer.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    target.classList.add('colored');
+})
